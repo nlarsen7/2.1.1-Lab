a66a90e2528176d6f14d08fa01e4423ad3c10d8e
nlarsen7 <nlarsen2013@gmail.com>
2020-02-19 03:15:01 +0000

    Lab 2.1.1

--- main.go
+++ main.go
@@ -1,4 +1,6 @@
-
+//Nicholas Larsen
+//February 18, 2020
+//Prompts the user to guess the computer's random number
 package main
 
 import (
@@ -11,26 +13,32 @@ func main() {
   var count int
 
   fmt.Println("Enter a max range for this guessing game")
-
-fmt.Scanln(&max)   
+  
+fmt.Scanln(&max)
+   //has the user set a max range for the random computer guess
     var computerGuess = rand.Intn(max)
     var userGuess int
 
     fmt.Println("Enter a guess for the computer number")
 
     fmt.Scanln(&userGuess)
+    //the user's guess for the number
 
     for userGuess!=computerGuess {
       fmt.Println("That number is incorrect, try again!")
 
       count++
+      //counts the tries
 
       fmt.Scanln(&userGuess)
+      //has user input another number
 
       if userGuess==computerGuess{
         fmt.Println("You've guessed correctly!")
+        //tells user they have guessed the number correctly
       }
     }
     fmt.Println("It took you",count,"times to guess the correct number")
+    //tells the user how many tries it took
 
 }
